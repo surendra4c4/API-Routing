223ba9ffb0281a78f1d85c8a442734430674521b
surendra4c4 <surendra4c4@gmail.com>
2021-12-25 12:22:37 +0000

    api routing

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.listen(3000);
+
+module.exports = app;
